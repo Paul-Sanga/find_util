62d9cfdabc3daf2f6099d5f13cbd73fd1fddb598
Paul-Sanga <paulsanganyamawi@gmail.com>
2024-04-03 16:23:53 +0300

    find util

--- src/lib.rs
+++ src/lib.rs
@@ -33,13 +33,25 @@ impl FindImpl {
 
     pub fn get_sub_dirs(&mut self, path: PathBuf) {
         match std::fs::read_dir(path) {
-            Ok(contents) =>{
-                
-            },
+            Ok(contents) => contents.into_iter().for_each(|el| match el {
+                Ok(entry) => {
+                    if entry.path().is_dir() {
+                        self.sub_dirs.push(entry.path())
+                    }
+                    if entry.path().is_file() {
+                        self.files.push(entry.path())
+                    }
+                }
+                Err(error) => {
+                    println!("\x1b[31m {error} \x1b[0m")
+                }
+            }),
             Err(error) => {
                 println!("\x1b[31m {error} \x1b[0m")
             }
         }
+        dbg!(&self.sub_dirs);
+        dbg!(&self.files);
     }
 
     fn populate(&mut self) {}
